9a63e7313cc2bc81e05cdd774aba6cf2544021a8
miloujb <emilyjbailey@hotmail.co.uk>
2019-11-18 13:36:33 +0000

    basic metronome function written

--- src/Components/Metronome.js
+++ src/Components/Metronome.js
@@ -2,9 +2,15 @@ import React, { Component } from "react";
 
 class Metronome extends Component {
   render() {
+    let bpm = 120;
+    let playing = false;
     return (
       <div className="metronome">
-        <h1>hi</h1>
+        <div className="slider">
+          <div>{bpm} BPM</div>
+          <input type="range" min="60" max="240" value={bpm} />
+        </div>
+        <button>{playing ? "Stop" : "Start"}</button>
       </div>
     );
   }
