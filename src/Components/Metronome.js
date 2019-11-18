323a6f8c33361cc9520bc9402ace1d4f6e897446
miloujb <emilyjbailey@hotmail.co.uk>
2019-11-18 13:31:33 +0000

    component created

---
+++ src/Components/Metronome.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Metronome extends Component {
+  render() {
+    return (
+      <div className="metronome">
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
+
+export default Metronome;
